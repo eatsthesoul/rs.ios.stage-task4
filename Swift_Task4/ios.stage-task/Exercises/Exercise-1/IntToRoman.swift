8d3494a29ba0eaa87f687a0e9f6590d6ae08af5a
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-06-06 17:29:03 +0300

    feat: add solution for exercise-1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,29 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        let romanArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        let arabicArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        
+        var number = self
+        var resultString = ""
+        
+        for (index, romanChar) in romanArray.enumerated() {
+            
+            let currentArabicValue = arabicArray[index]
+            
+            let valuesInNumberCounter = number / currentArabicValue
+            
+            if valuesInNumberCounter > 0 {
+                
+                for _ in 0..<valuesInNumberCounter {
+                    resultString += romanChar
+                }
+                
+                number -= currentArabicValue * valuesInNumberCounter
+            }
+        }
+
+        return (resultString != "") ? resultString : nil
     }
 }
